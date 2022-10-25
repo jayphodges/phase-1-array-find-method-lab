cef1d341285bbda14232c53c87c17d747da42dda
Jay P Hodges <jayphodges@gmail.com>
2022-10-25 16:38:34 -0600

    commit tree d8dce9611759b08d5186641ed70e4286edae4685

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(array){
+    const results = array.find(findWin);
+    if (!!results === true) {
+        return array.find(findWin).year
+    } else {
+        return undefined
+    }
+}
+function findWin(game) {
+    return game.result === 'W'
+}
